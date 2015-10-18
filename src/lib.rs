94649f5e9f188f89dfa9c08026dc21aa39c03761
Mike Piccolo <mfpiccolo@gmail.com>
2015-10-17 19:46:35 -0700

    Make print_type_of public extern

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,10 @@
 
 #[macro_export]
 macro_rules! t {
-  ($from:expr) => (self::print_type_of($from));
+  ($from:expr) => (print_type_of($from));
 }
 
-
-fn print_type_of<'a, T>(_: T) -> &'a str {
+pub extern fn print_type_of<'a, T>(_: T) -> &'a str {
   let type_name = unsafe { std::intrinsics::type_name::<T>() };
   type_name
 }
