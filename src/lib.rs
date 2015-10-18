b476dcca9c3c143ec01d8bee93e4a3bbaa26a584
Mike Piccolo <mfpiccolo@gmail.com>
2015-10-17 19:55:28 -0700

    Add print type macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,14 @@
 
 #[macro_export]
 macro_rules! t {
-  ($from:expr) => (print_type_of($from));
+  ($from:expr) => (type_of($from));
 }
 
-pub extern fn print_type_of<'a, T>(_: T) -> &'a str {
-  let type_name = unsafe { std::intrinsics::type_name::<T>() };
-  type_name
+#[macro_export]
+macro_rules! pt {
+  ($from:expr) => (println!(type_of($from)));
+}
+
+pub extern fn type_of<'a, T>(_: T) -> &'a str {
+  unsafe { std::intrinsics::type_name::<T>() }
 }
