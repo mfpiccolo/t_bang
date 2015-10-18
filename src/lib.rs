9462064160a3ea34301481fed5cf7bcfeeceb52f
Mike Piccolo <mfpiccolo@gmail.com>
2015-10-17 19:20:14 -0700

    Move function into macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 #![feature(core_intrinsics)]
-#![macro_use]
 
 #[macro_export]
 macro_rules! t {
-  ($from:expr) => (print_type_of($from));
+  ($from:expr) => (unsafe { std::intrinsics::type_name::<T>() } as &str);
 }
 
 fn print_type_of<'a, T>(_: T) -> &'a str {
-  let type_name = unsafe { std::intrinsics::type_name::<T>() } as &str;
-  type_name
+  unsafe { std::intrinsics::type_name::<T>() } as &str;
 }
 
 pub extern fn something() {
